457241de84669a3382b5c1f259cdb506769453dc
muhammet celepci <muhammetcelepci1@gmail.com>
2020-01-16 02:13:47 +0300

    Online api düzeltildi

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   env: {
     TEST: process.env.TEST,
-    URL: isProd ? 'https://muhammetcelepci.herokuapp.com/' : 'http://localhost:3000',
+    URL: isProd ? 'http://muhammetcelepci.herokuapp.com' : 'http://localhost:3000',
   },
   webpack: config => {
     config.module.rules.push({
